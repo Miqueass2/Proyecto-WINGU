21ddd8debc8c5603747aa9b6ca9c3ae1f612a5e7
Miqueas <miqueaslazarte03@gmail.com>
2023-05-31 12:08:43 -0300

    agregue cositas

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
   
 (function main() { 
   const submitMoney = document.querySelector('#submit-money');
-  console.log("im submi",submitMoney);
+
   const submitEspecie = document.querySelector('#submit-especie');
-  console.log("im input",submitEspecie);
+
   const submitVoluntary = document.querySelector('#submit-voluntary');
-  console.log("im ul", submitVoluntary);
+
+  const productInterest = document.querySelector('#product_interest');
   
   
   submitMoney.addEventListener('click', (e) => {
     e.preventDefault();
     const selectedDiv = document.querySelector('#selected-div');
+    
+    productInterest.value = 'Donación Monetaria';
+    console.log("inputvalue::",productInterest.value);
     selectedDiv.innerHTML = `<p class="p-selected">Seleccionaste Donación Monetaria</p>`;
   });
   submitEspecie.addEventListener('click', (e) => {
     e.preventDefault();
     const selectedDiv = document.querySelector('#selected-div');
+    productInterest.value = 'Donación en Especie';
     selectedDiv.innerHTML = `<p class="p-selected">Seleccionaste Donación en Especie</p>`;
   });
   submitVoluntary.addEventListener('click', (e) => {
     e.preventDefault();
     const selectedDiv = document.querySelector('#selected-div');
+    productInterest.value = 'Voluntariado';
     selectedDiv.innerHTML = `<p class="p-selected">Seleccionaste Voluntariado</p>`;
   });
 })();  
